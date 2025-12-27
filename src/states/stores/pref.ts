3146a372ac66e4033228f1e15ae6aafc809ab9e1
Linho1219 <linhongping1219@163.com>
2025-12-27 18:41:17 +0800

    默认禁用逐字音译

--- src/states/stores/pref.ts
+++ src/states/stores/pref.ts
@@ -14,7 +14,7 @@ export const usePrefStore = defineStore('preference', () => {
   const hotkeyMap = reactive(getDefaultHotkeyMap())
   const isMac = ref(isAppleDevice())
   const packAudioToProject = ref(true)
-  const showSylLvlRoman = ref(true)
+  const showSylLvlRoman = ref(false)
 
   return {
     globalLatency,
